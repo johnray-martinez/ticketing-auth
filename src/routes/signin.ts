242d1f562a82bdbb6b6b6ebc074d44d7ab0b73af
johnray-martinez <johnraymartinezwebdev@gmail.com>
2023-07-10 14:00:52 -0400

    fixed bug with the password helper. Added 'hex' to the toString() function

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,9 +1,58 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { body, validationResult } from 'express-validator';
+
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models/user';
+import { Password } from '../services/password';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.get('/api/users/signin', (req, res) => {
-  res.send('This is a signin');
-})
+router.post('/api/users/signin',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password')
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('No account with that email had been created.');
+    }
+
+    // check if password is valid
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid email or password.');
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+      id: existingUser.id,
+      email: existingUser.email
+    }, process.env.jwt!);
+    
+    // Store it on session object
+    req.session = { 
+      jwt: userJwt 
+    };
+
+    return res.status(201).send(existingUser);
+  }
+);
 
 export { router as signinUserRouter };
--- src/services/password.ts
+++ src/services/password.ts
@@ -15,6 +15,6 @@ export class Password {
     const [hashedPassword, salt] = storedPassword.split('.');
     const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
 
-    return buffer.toString() === hashedPassword;
+    return buffer.toString('hex') === hashedPassword;
   }
 }
